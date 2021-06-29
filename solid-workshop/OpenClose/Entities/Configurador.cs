b8cc39283a013b562dddd3d18ddd9d7dfe555a2b
Igor Luna <igor.luna@jsl.com.br>
2021-06-28 22:02:58 -0300

    addin missing files for openclose

---
+++ solid-workshop/OpenClose/Entities/Configurador.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+public class Configurador
+{
+    internal ConfigSMTP ObtemConfigSMTP()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal string ObtemEmailCliente()
+    {
+        throw new NotImplementedException();
+    }
+
+ 
+}
+public class ConfigSMTP{}
+public class Repositorio
+{
+    internal string ObtemUltimaIntegracao()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ solid-workshop/OpenClose/Problem.cs
@@ -0,0 +1,50 @@
+public class Sistema
+{
+    private IntegradorEmail _integrador = new IntegradorEmail(
+        new Configurador(),
+        new Repositorio());
+
+    public Sistema(IntegradorEmail integrador)
+    {
+        _integrador = integrador;
+    }
+    public void Principal()
+    {
+        _integrador.Integrar();
+    }
+}
+
+public class IntegradorEmail
+{
+    private Configurador _configurador;
+    private Repositorio _repositorio;
+
+    public IntegradorEmail(
+        Configurador configurador,
+        Repositorio repositorio)
+    {
+        _configurador = configurador;
+        _repositorio = repositorio;
+    }
+    
+    public bool Integrar()
+    {
+        // Obtem email destino
+        string emailDestino = _configurador.ObtemEmailCliente();
+
+        // Obtem configuracao SMTP
+        ConfigSMTP configSmtp = _configurador.ObtemConfigSMTP();
+
+        // Obtem a utlima mensagem 
+        string mensagem = _repositorio.ObtemUltimaIntegracao();
+
+        // Envia o email
+        return EnviarEmail(configSmtp, emailDestino, mensagem);
+    }
+
+    private bool EnviarEmail(ConfigSMTP configSmtp, string emailDestino, string mensagem)
+    {
+        // Enviou o email
+        return true;
+    }
+}
