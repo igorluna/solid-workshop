4c6c5ba2dd491cf09bd177192d789dab8b606fe7
Igor Luna <igor.luna@jsl.com.br>
2021-06-28 22:02:43 -0300

    Adding openclose principle example

---
+++ solid-workshop/OpenClose/Solution.cs
@@ -0,0 +1,65 @@
+public class SistemaNovo
+{
+    private IIntegrador _integrador = new IntegradorEmailNovo(
+        new Configurador(),
+        new Repositorio());
+
+    // private IIntegrador _integrador = new IntegradorApi();
+
+    public SistemaNovo(IIntegrador integrador)
+    {
+        _integrador = integrador;
+    }
+    public void Principal()
+    {
+        _integrador.Integrar();
+    }
+}
+
+public class IntegradorEmailNovo : IIntegrador
+{
+    private Configurador _configurador;
+    private Repositorio _repositorio;
+
+    public IntegradorEmailNovo(
+        Configurador configurador,
+        Repositorio repositorio)
+    {
+        _configurador = configurador;
+        _repositorio = repositorio;
+    }
+    
+    public bool Integrar()
+    {
+        // Obtem email destino
+        string emailDestino = _configurador.ObtemEmailCliente();
+
+        // Obtem configuracao SMTP
+        ConfigSMTP configSmtp = _configurador.ObtemConfigSMTP();
+
+        // Obtem a utlima mensagem 
+        string mensagem = _repositorio.ObtemUltimaIntegracao();
+
+        // Envia o email
+        return EnviarEmail(configSmtp, emailDestino, mensagem);
+    }
+
+    private bool EnviarEmail(ConfigSMTP configSmtp, string emailDestino, string mensagem)
+    {
+        // Enviou o email
+        return true;
+    }
+}
+
+public class IntegradorApi : IIntegrador
+{
+    public bool Integrar()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public interface IIntegrador { 
+    bool Integrar();
+}
+
+
